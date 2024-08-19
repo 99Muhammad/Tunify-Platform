afb6e7b066ac58a56cbcbba6d4c105263f5659b9
99Muhammad <MuhammadIbraheem99m@gmail.com>
2024-08-19 22:36:09 +0300

    Swagger

--- Tunify/Program.cs
+++ Tunify/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Tunify.Data;
+using Tunify.Model;
 using Tunify.Repositories.Interfaces;
 using Tunify.Repositories.Services;
 
@@ -12,34 +13,70 @@ namespace Tunify
         public static void Main(string[] args)
         {
 
+            /* var builder = WebApplication.CreateBuilder(args);
+
+
+             builder.Services.AddControllers();
+
+             var ConnectionString = builder.Configuration.GetConnectionString("constr");
+
+             builder.Services.AddDbContext<TunifyDbContext>(options =>
+             options.UseSqlServer(ConnectionString));
+
+
+             //builder.Services.AddScoped<IPlayList, PlayListService>();
+
+             builder.Services.AddScoped<IPlayList, PlayListService>();
+             builder.Services.AddScoped<IUsers, UserService>();
+             builder.Services.AddScoped<ISong, SongService>();
+             builder.Services.AddScoped<IArtist, ArtistService>();
+
+             var app = builder.Build();
+
+
+
+
+
+             app.MapControllers();
+
+             app.MapGet("/", () => "Hello World!");
+
+             app.Run();*/
+
             var builder = WebApplication.CreateBuilder(args);
-           
-            
-            builder.Services.AddControllers();
-            
-            var ConnectionString = builder.Configuration.GetConnectionString("constr");
-
             builder.Services.AddDbContext<TunifyDbContext>(options =>
-            options.UseSqlServer(ConnectionString));
-
-           
-            //builder.Services.AddScoped<IPlayList, PlayListService>();
-
-            builder.Services.AddScoped<IPlayList, PlayListService>();
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddControllers();
             builder.Services.AddScoped<IUsers, UserService>();
             builder.Services.AddScoped<ISong, SongService>();
             builder.Services.AddScoped<IArtist, ArtistService>();
-
+            builder.Services.AddScoped<IPlayList, PlayListService>();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Tunify API",
+                    Version = "v1",
+                    Description = "API for managing playlists, songs, and artists in the Tunify Platform"
+                });
+            });
             var app = builder.Build();
+            app.UseSwagger(
+             options =>
+             {
+                 options.RouteTemplate = "api/{documentName}/swagger.json";
+             }
+);
 
 
-
-
-
-
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/api/v1/swagger.json", "Tunify API v1");
+                options.RoutePrefix = "";
+            });
             app.MapControllers();
-            
-            app.MapGet("/", () => "Hello World!");
+
+            //app.MapGet("/", () => "Hello World!");
 
             app.Run();
         }
